b60921ec734d38448e4142bc591770ca84ca989d
Lucas Bisoffi <lucasdb3301@gmail.com>
2026-01-10 14:16:25 -0300

    get_id removed

--- scraper.py
+++ scraper.py
@@ -14,63 +14,56 @@ def enviar_telegram(mensagem):
     data = {'chat_id': CHAT_ID, 'text': mensagem}
     requests.post(url_api, data=data)
 
-# INICIO DO SCRAPER
+def scrap_python_org():
+    url = 'https://www.python.org/jobs'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac Os X 10_15_7)'}
+    response = requests.get(url, headers=headers)
+    print(f'Status da resposta do site: {response.status_code}')
+    lista_vagas_python = []
 
-url = 'https://www.python.org/jobs'
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        lista_de_vagas = soup.find('ol', class_ = 'list-recent-jobs')
+        todas_as_vagas = lista_de_vagas.find_all('li')
+        print(f'Site acessado! Processando {len(todas_as_vagas)} vagas...\n')
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac Os X 10_15_7)'
-}
-print('Iniciando VagaBot...')
-response = requests.get(url, headers=headers)
+        for vaga in todas_as_vagas:
+            titulo_tag = vaga.find('a')
+            local_tag = vaga.find('span', class_='listing-location')
 
-print(f'Status da resposta do site: {response.status_code}')
+            if titulo_tag and local_tag:
+                titulo = titulo_tag.text.strip()
+                local = local_tag.text.strip()
+                link = 'https://www.python.org' + titulo_tag['href']
+                local_lower = local.lower()
 
-# 200: sucesso
-# 403: negado
-# 404: nao encontrado
+                #filtrando apenas vagas no Brasil:
+                if 'brazil' in local_lower:
+                    vaga_dict = {
+                        'Cargo': titulo,
+                        'Localização': local,
+                        'Link': link 
+                    }
+                    lista_vagas_python.append(vaga_dict)
+    else:
+        print('Erro ao acessar o site.')
 
-print(f'Status da resposta: {response.status_code}')
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    lista_de_vagas = soup.find('ol', class_ = 'list-recent-jobs')
-    todas_as_vagas = lista_de_vagas.find_all('li')
+    return lista_vagas_python #cada vaga tem seu dict
 
-    dados_vagas = []
-    print(f'Site acessado! Processando {len(todas_as_vagas)} vagas...\n')
 
-    for vaga in todas_as_vagas:
-        titulo_tag = vaga.find('a')
-        local_tag = vaga.find('span', class_='listing-location')
+if __name__ == "__main__":
+    vagas_total = []
+    vagas_total.extend(scrap_python_org())
+    #vagas_total.extend(scrap_sites_sjc())
 
-        if titulo_tag and local_tag:
-            titulo = titulo_tag.text.strip()
-            local = local_tag.text.strip()
-            link = 'https://www.python.org' + titulo_tag['href']
-
-            #filtrar vagas que sejam no brasil, remotas ou que nao sejam senior/lead
-            titulo_lower = titulo.lower()    
-            local_lower = local.lower()
-
-            #if 'brazil' in local_lower or 'remote' in local_lower:
-            if 'brazil' in local_lower:
-                msg_final = f"🔥 VAGA ENCONTRADA!\n\nCargo: {titulo}\nLocal: {local}\nLink: {link}"
-                vaga_dict = {
-                    'Cargo': titulo,
-                    'Localização': local,
-                    'Link': link 
-                }
-                dados_vagas.append(vaga_dict)
-                print(f"Enviando alerta: {titulo}...")
-                enviar_telegram(msg_final)
-
-                time.sleep(1) #recurso pro telegram não bloquear as solicitações
-        
-    if dados_vagas:
-        df = pd.DataFrame(dados_vagas)
+    if vagas_total:
+        df = pd.DataFrame(vagas_total)
         df.to_csv('vagas_encontradas.csv', index=False)
+        for vaga in vagas_total:
+            msg_final = f"🔥 VAGA ENCONTRADA!\n\nCargo: {vaga['Cargo']}\nLocal: {vaga['Localização']}\nLink: {vaga['Link']}"
+            print(f"Enviando alerta: {vaga['Cargo']}...")
+            enviar_telegram(msg_final)                
+            time.sleep(1) #recurso pro telegram não bloquear as solicitações
         print(f'\nSucesso! Verifique seu Telegram e o arquivo CSV.')
     else:
         print('Nenhuma vaga correspondeu aos filtros.')
-
-else:
-    print('Erro ao acessar o site.')
